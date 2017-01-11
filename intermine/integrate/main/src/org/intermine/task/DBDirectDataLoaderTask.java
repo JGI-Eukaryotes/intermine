bd6d15928f68ea8edb78116d0136ff0c7bf2f786
JoeCarlson <JWCarlson@lbl.gov>
2017-01-11 13:48:24 -0800

    restoring deleted file

---
+++ intermine/integrate/main/src/org/intermine/task/DBDirectDataLoaderTask.java
@@ -0,0 +1,53 @@
+package org.intermine.task;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.tools.ant.BuildException;
+import org.intermine.sql.Database;
+import org.intermine.sql.DatabaseFactory;
+
+public abstract class DBDirectDataLoaderTask extends DirectDataLoaderTask {
+
+  private String dbName;
+  private Connection connection;
+  private Database db = null;
+  
+  protected Database getDb() {
+    if (db==null) {
+      if (dbName == null ) {
+        throw new BuildException("dbName is not set.");
+      }
+      try {
+        db = DatabaseFactory.getDatabase(dbName);
+      } catch (ClassNotFoundException | SQLException e) {
+        throw new BuildException("Trouble getting database: " +e.getMessage());
+      }     
+    }
+    return db;
+  }
+  
+  protected Connection getConnection() {
+    if (connection == null ) {
+      try {
+        connection = getDb().getConnection();
+      } catch (SQLException e) {
+        throw new BuildException("Trouble getting database connection: " +e.getMessage());
+      }
+    }
+    return connection;
+  }
+  
+  /**
+   * Set the Database to read from
+   * @param dbName the database alias
+   */
+  
+  public void setSourceDbName(String dbName) {
+    if (dbName.startsWith("db.")) {
+      this.dbName = dbName;
+    } else {
+      this.dbName = "db."+dbName;
+    }
+  }
+}
